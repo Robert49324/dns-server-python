bffee3e4ef49a4e2041a0b8bc83f06403c2773bf
JustPC <robert49324@gmail.com>
2023-11-26 16:03:06 +0300

    pass 3d stage

--- app/main.py
+++ app/main.py
@@ -39,9 +39,8 @@ def pack_dns_message(message: DNSMessage) -> bytes:
     return struct.pack(">HHHHHH", message.id, flags, message.qdcount, message.ancount, message.nscount, message.arcount)
 
 def pack_dns_question(message: DNSQuestion) -> bytes:
-    print(message.name)
-    print(b"%s" %message.name)
-    return struct.pack(f">HH{len(message.name)}s", message.type_, message.class_, message.name.encode())
+
+    return struct.pack(f">HH{len(message.name)}s", message.type_, message.class_, message.name)
 
 
 
