689bfa2441cb83c68470ce7c64ac9452f7ec5d41
JustPC <robert49324@gmail.com>
2023-11-26 15:30:12 +0300

    pass 3d stage

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ class DNSMessage:
     
 @dataclass
 class DNSQuestion:
-    type: int
+    type_: int
     class_: int
     name: int
 
@@ -39,12 +39,8 @@ def pack_dns_message(message: DNSMessage) -> bytes:
     return struct.pack(">HHHHHH", message.id, flags, message.qdcount, message.ancount, message.nscount, message.arcount)
 
 def pack_dns_question(message: DNSQuestion) -> bytes:
-    flags = (
-        (message.type << 15)
-        | (message.class_ << 11)
-        | (message.name << 10)
-    )
-    return struct.pack(">HHH", message.type, flags, message.class_, message.name)
+
+    return struct.pack(">HHH", message.type_.to_bytes(2,byteorder="big"), message.class_.to_bytes(2,byteorder="big"), message.name.to_bytes(2,byteorder="big"))
 
 
 
@@ -74,15 +70,15 @@ def main():
                     arcount=0,
                 ))
             
-            
-            num = 1
-            Type = num.to_bytes(2,byteorder="big")
+            # Type = int(1).to_bytes(2,byteorder="big")
 
-            Class = int(1).to_bytes(2, byteorder='big')  
+            # Class = int(1).to_bytes(2, byteorder='big')  
 
             Name = b'\x0ccodecrafters\x02io\x00'
+            
+            Question = pack_dns_question(DNSQuestion(1,1,Name))
 
-            Question = pack_dns_question(DNSQuestion(1,1,b"\x0ccodecrafters\x02io\x00"))
+            # Question = Name + Type + Class
     
             udp_socket.sendto(response + Question, source)
         except Exception as e:
