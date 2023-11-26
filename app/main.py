dbe493c62c3792dc9e9054abe4269cd8b0fe42f2
JustPC <robert49324@gmail.com>
2023-11-26 16:26:33 +0300

    pass 3d stage

--- app/main.py
+++ app/main.py
@@ -17,12 +17,6 @@ class DNSMessage:
     ancount: int
     nscount: int
     arcount: int
-    
-# @dataclass
-# class DNSQuestion:
-    # type_: int
-    # class_: int
-    # name: str
 
 
 def pack_dns_message(message: DNSMessage) -> bytes:
@@ -37,12 +31,6 @@ def pack_dns_message(message: DNSMessage) -> bytes:
         | message.rcode
     )
     return struct.pack(">HHHHHH", message.id, flags, message.qdcount, message.ancount, message.nscount, message.arcount)
-
-# def pack_dns_question(message: DNSQuestion) -> bytes:
-#     name_bytes = message.name.encode('utf-8')
-#     return struct.pack(f">HH{len(name_bytes)}s", message.type_, message.class_, name_bytes)
-
-
 
 
 
@@ -72,13 +60,13 @@ def main():
                     arcount=0,
                 ))
             
-            Type = int(1).to_bytes(2,byteorder="big")
+            
+            num = 1
+            Type = num.to_bytes(2,byteorder="big")
 
             Class = int(1).to_bytes(2, byteorder='big')  
 
-            Name = '\x0ccodecrafters\x02io\x00'
-            
-            # Question = pack_dns_question(DNSQuestion(1,1,Name))
+            Name = b'\x0ccodecrafters\x02io\x00'
 
             Question = Name + Type + Class
     
