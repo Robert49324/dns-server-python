b3009895c12d201ac0ca0ea141f9e2439163de6a
JustPC <robert49324@gmail.com>
2023-12-02 11:51:40 +0300

    pass 3d stage

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import socket
 from dataclasses import dataclass
 import struct
 
+
 @dataclass
 class DNSMessage:
     id: int
@@ -18,7 +19,6 @@ class DNSMessage:
     nscount: int
     arcount: int
 
-
     def pack_dns_message(self) -> bytes:
         flags = (
             (self.qr << 15)
@@ -32,6 +32,7 @@ class DNSMessage:
         )
         return struct.pack(">HHHHHH", self.id, flags, self.qdcount, self.ancount, self.nscount, self.arcount)
 
+
 class Question:
     def __init__(self, name, typ, cls):
         self.Name = name
@@ -40,6 +41,7 @@ class Question:
     Name: str
     Type: int
     Class: int
+
     def build(self) -> bytes:
         names = self.Name.split(".")
         bytes_array = b""
@@ -49,6 +51,7 @@ class Question:
         bytes_array += b"\x00" + struct.pack(">HH", self.Type, self.Class)
         return bytes_array
 
+
 class Answer:
     Name: str
     Type: int
@@ -56,6 +59,7 @@ class Answer:
     TTL: int
     Length: int
     Data: str
+
     def __init__(self, name, typ, cls, ttl, length, data):
         self.Name = name
         self.Type = typ
@@ -63,6 +67,7 @@ class Answer:
         self.TTL = ttl
         self.Length = length
         self.Data = data
+
     def build(self) -> bytes:
         bytes_array = b""
         names = self.Name.split(".")
@@ -74,29 +79,28 @@ class Answer:
         )
         return bytes_array
 
+
 def main():
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.bind(("127.0.0.1", 2053))
-    
+
     while True:
         try:
-
             buf, source = udp_socket.recvfrom(1024)
-            
+            print(buf)
             id = struct.unpack('!H', buf[:2])[0]
-            byte = struct.unpack('!B', buf[2:3])[0] 
-            qr = byte >> 7 
-            op = (byte >> 3) & 0b1111 
+            byte = struct.unpack('!B', buf[2:3])[0]
+            qr = byte >> 7
+            op = (byte >> 3) & 0b1111
             rd = byte & 1
-            
-            print(id)
-            
+
             response = DNSMessage(
                 id, 1, op, 0, 0, rd, 0, 0, 0 if op == 0 else 4, 1, 1, 0, 0
             ).pack_dns_message()
             response += Question("codecrafters.io", 1, 1).build()
 
-            response += Answer("codecrafters.io", 1, 1, 60, 4, "8.8.8.8").build()
+            response += Answer("codecrafters.io", 1, 1,
+                               60, 4, "8.8.8.8").build()
             udp_socket.sendto(response, source)
         except Exception as e:
             print(f"Error receiving data: {e}")
