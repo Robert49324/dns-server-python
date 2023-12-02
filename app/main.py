6ac9eaaec7b16d492af95b7e4726426ee6a9df2a
JustPC <robert49324@gmail.com>
2023-12-02 12:26:52 +0300

    pass 3d stage

--- app/main.py
+++ app/main.py
@@ -121,9 +121,9 @@ def main():
             response = DNSMessage(
                 id, 1, op, 0, 0, rd, 0, 0, 0 if op == 0 else 4, 1, 1, 0, 0
             ).pack_dns_message()
-            response += Question("codecrafters.io", 1, 1).build()
+            response += Question(domain, 1, 1).build()
 
-            response += Answer("codecrafters.io", 1, 1,
+            response += Answer(domain, 1, 1,
                                60, 4, "8.8.8.8").build()
             udp_socket.sendto(response, source)
         except Exception as e:
